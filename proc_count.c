d90ab45e7601a8e001898453e7125fe6d78aa9b0
Alex West <awest2025@ucla.edu>
2022-09-28 17:10:35 -0700

    initial commit

---
+++ proc_count.c
@@ -0,0 +1,24 @@
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/sched.h>
+
+static int __init proc_count_init(void)
+{
+	pr_info("proc_count: init\n");
+	// proc_create("proc_count", 0, NULL, &proc_count_show);
+	return 0;
+}
+
+static void __exit proc_count_exit(void)
+{
+	pr_info("proc_count: exit\n");
+}
+
+module_init(proc_count_init);
+module_exit(proc_count_exit);
+
+MODULE_AUTHOR("Alex West");
+MODULE_DESCRIPTION("Lists the number of processes");
+MODULE_LICENSE("GPL");
